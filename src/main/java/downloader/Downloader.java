10757c9d7098469b7122c095361fefacb3c949e6
Rajat Chandak <rajchandak1993@gmail.com>
2017-09-02 16:18:05 +0530

    basic download feature done

--- src/main/java/downloader/Downloader.java
+++ src/main/java/downloader/Downloader.java
@@ -1,10 +1,37 @@
 package downloader;
 
+import console.Display;
+import io.Reader;
+import io.Writer;
+import java.io.RandomAccessFile;
+import java.util.Optional;
+
 public class Downloader {
+    private StateManager state;
+    private Reader reader;
+    private Writer writer;
 
-    public Downloader() {
+    public Downloader(StateManager state,Reader reader, Writer writer) {
+        this.state = state;
+        this.reader = reader;
+        this.writer = writer;
     }
 
-    public void download(String url)  {
+    public Optional<RandomAccessFile> download() {
+        while (state.isDownloading()) {
+            byte [] buffer = new byte[4096];
+            int bytesRead = reader.readContent(buffer);
+            if (noMoreContentAvailable(bytesRead))
+                break;
+            state.trackContentDownloaded(bytesRead);
+            Display.progress();
+            writer.write(bytesRead,buffer);
+        }
+        return writer.getRandomAccessFile();
     }
+
+    private boolean noMoreContentAvailable(int bytesRead) {
+        return bytesRead == -1;
+    }
+
 }
--- src/test/java/downloader/DownloaderTest.java
+++ src/test/java/downloader/DownloaderTest.java
@@ -1,5 +1,61 @@
 package downloader;
 
+import io.Reader;
+import io.Writer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DownloaderTest {
+
+    private Downloader downloader;
+
+    @Mock
+    private Reader reader;
+
+    @Mock
+    private Writer writer;
+
+    @Mock
+    private StateManager stateManager;
+
+    @Before
+    public void setUp() {
+        downloader = new Downloader(stateManager, reader, writer);
+    }
+
+    @Test
+    public void shouldBeAbleToDownloadTheFile() throws IOException {
+        Optional<RandomAccessFile> randomAccessFile = Optional.of(new RandomAccessFile("src/test/resources/sample.txt", "rw"));
+        byte[] buffer = new byte[4096];
+        int bytesRead = 2000;
+        int endBytesRead = -1;
+        int fileSize = 3278;
+
+        when(stateManager.isDownloading()).thenReturn(true);
+        when(reader.readContent(buffer)).thenReturn(bytesRead).thenReturn(endBytesRead);
+
+        doNothing().when(stateManager).trackContentDownloaded(bytesRead);
+        doNothing().when(writer).write(bytesRead, buffer);
+        when(writer.getRandomAccessFile()).thenReturn(randomAccessFile);
+
+        downloader.download();
+        verify(stateManager,times(2)).isDownloading();
+        verify(reader,times(2)).readContent(buffer);
+        verify(stateManager,times(1)).trackContentDownloaded(bytesRead);
+        verify(writer,times(1)).write(bytesRead,buffer);
+
+        assertEquals(randomAccessFile.get().length(), fileSize);
+    }
 
 }
